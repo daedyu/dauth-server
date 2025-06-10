e15edfcc790b1f3d20095d1c827e4111933cca2c
kmg <legolove08@naver.com>
2025-06-10 23:41:02 +0900

    chore: field

--- src/main/kotlin/com/b1nd/dauthserver/application/token/data/TokenResponse.kt
+++ src/main/kotlin/com/b1nd/dauthserver/application/token/data/TokenResponse.kt
@@ -1,14 +1,8 @@
 package com.b1nd.dauthserver.application.token.data
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class TokenResponse(
-    @field:JsonProperty("access_token")
     val accessToken: String,
-    @field:JsonProperty("refresh_token")
     val refreshToken: String,
-    @field:JsonProperty("id_token")
     val idToken: String,
-    @field:JsonProperty("token_type")
     val tokenType: String = "Bearer"
 )
